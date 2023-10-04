9df3d8369252d01d1598097ebb8843b43b16f333
Rizky Faturriza <rfaturriza@gmail.com>
2023-10-04 22:10:52 +0700

    update: YoutubePlayer.kt

--- app/src/main/java/com/rizz/mandiri/assignment/features/movie/presentation/movieDetailScreen/components/YoutubePlayer.kt
+++ app/src/main/java/com/rizz/mandiri/assignment/features/movie/presentation/movieDetailScreen/components/YoutubePlayer.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.LifecycleOwner
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
@@ -17,20 +15,16 @@ fun YoutubeScreen(
     videoId: String,
     modifier: Modifier = Modifier
 ) {
-    val ctx = LocalContext.current
-    val lifecycle = (ctx as LifecycleOwner).lifecycle
     Box(
         modifier = modifier.fillMaxSize()
     ) {
         AndroidView(factory = {
-            val view = YouTubePlayerView(it).apply {
-                lifecycle.addObserver(this)
-            }
+            val view = YouTubePlayerView(it)
             view.addYouTubePlayerListener(
                 object : AbstractYouTubePlayerListener() {
                     override fun onReady(youTubePlayer: YouTubePlayer) {
                         super.onReady(youTubePlayer)
-                        youTubePlayer.loadVideo(videoId, 0f)
+                        youTubePlayer.cueVideo(videoId, 0f)
                     }
                 }
             )
